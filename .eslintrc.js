44450feff7228d0f3fbde06e9d67e409f0fd9d5f
pavel.and.ivanov <pavel.and.ivanov@megafon.ru>
2023-01-09 12:44:40 +0300

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,18 @@
 module.exports = {
    "env": {
       "browser": true,
-      "es2021": true
+      "es2021": true,
+      "node": true,
+   },
+   "settings": {
+      "react": {
+         "version": "detect"
+      }
    },
    "extends": [
       "eslint:recommended",
       "plugin:react/recommended",
+      "plugin:react/jsx-runtime",
       "plugin:@typescript-eslint/recommended"
    ],
    "parser": "@typescript-eslint/parser",
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import AppHeader from "./AppHeader/AppHeader";
 import BurgerIngredients from "./BurgerIngredients/BurgerIngredients";
 import BurgerConstructor from "./BurgerConstructor/BurgerConstructor";
